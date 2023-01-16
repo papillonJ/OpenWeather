e6f0b516a8aeda17be5e7c32605fd775703ddf2a
Jeremy Wang <zwang232@hawk.iit.edu>
2023-01-16 15:14:30 -0600

    Project finished

---
+++ app/src/main/java/common/openWeatherApp/CityPreference.java
@@ -0,0 +1,24 @@
+package common.openWeatherApp;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+ 
+public class CityPreference {
+     
+    SharedPreferences prefs;
+     
+    public CityPreference(Activity activity){
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+     
+
+    public String getCity(){
+
+        return prefs.getString("city", "Chicago, IL");
+    }
+     
+    void setCity(String city){
+        prefs.edit().putString("city", city).commit();
+    }
+     
+}
